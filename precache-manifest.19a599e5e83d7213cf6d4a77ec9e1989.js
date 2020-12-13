10671cb843c5db6ce4195e4276a79c4ab71d85e0
dev-woong <occhoi90@gmail.com>
2020-12-13 21:47:07 +0900

    Updates

---
+++ precache-manifest.19a599e5e83d7213cf6d4a77ec9e1989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c4e182ea08169e2de053af5c573066",
+    "url": "/Clone_Naver_Memo/index.html"
+  },
+  {
+    "revision": "df32ee4a584d8f99a3f3",
+    "url": "/Clone_Naver_Memo/static/css/main.58f6055a.chunk.css"
+  },
+  {
+    "revision": "812d4ab977d6f94aa0c6",
+    "url": "/Clone_Naver_Memo/static/js/2.eddeec1b.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/Clone_Naver_Memo/static/js/2.eddeec1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df32ee4a584d8f99a3f3",
+    "url": "/Clone_Naver_Memo/static/js/main.0aa0f697.chunk.js"
+  },
+  {
+    "revision": "af91107c85665c1bde64",
+    "url": "/Clone_Naver_Memo/static/js/runtime-main.e1ac14d9.js"
+  }
+]);
