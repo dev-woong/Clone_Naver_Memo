b084f692c3196065e3c31d3a488d4a79048b4674
dev-woong <occhoi90@gmail.com>
2021-01-04 17:16:19 +0900

    add : add to getUserInfo function

--- src/components/layout/main/main.js
+++ src/components/layout/main/main.js
@@ -1,38 +1,40 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import List from '../../list/list';
-import Footer from '../footer/footer';
-import Header from '../header/header';
-import Sidebar from '../sidebar/sidebar';
-import styles from './main.module.css';
+import React from "react"
+import { useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import List from "../../list/list"
+import Footer from "../footer/footer"
+import Header from "../header/header"
+import Sidebar from "../sidebar/sidebar"
+import styles from "./main.module.css"
 
 const Main = ({ list, status, authService }) => {
-    const history = useHistory();
-    const onLogout = () => {
-        authService.onLogout();
-    }
+  const history = useHistory()
+  const onLogout = () => {
+    authService.onLogout()
+  }
 
-    useEffect(() => {
-        authService.onAuthChange(user => {
-            if (!user) {
-                history.push('/')
-            }
-        })
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        history.push("/")
+      } else {
+        console.log(authService.getUserInfo())
+      }
     })
-    return (
-        <section className={styles.main}>
-            <Header onLogout={onLogout} />
-            <section className={styles.container}>
-                <Sidebar />
-                <section className={styles.rightContainer}>
-                    <List list={list} />
-                </section>
+  })
 
-            </section>
-            <Footer isLogin={true} />
+  return (
+    <section className={styles.main}>
+      <Header onLogout={onLogout} />
+      <section className={styles.container}>
+        <Sidebar />
+        <section className={styles.rightContainer}>
+          <List list={list} />
         </section>
-    );
-};
+      </section>
+      <Footer isLogin={true} />
+    </section>
+  )
+}
 
-export default Main;
+export default Main
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -16,6 +16,18 @@ class AuthService {
   onLogout() {
     firebase.auth().signOut()
   }
+
+  getUserInfo() {
+    const firebaseUser = firebase.auth().currentUser
+    const user = {
+      name: firebaseUser.displayName,
+      email: firebaseUser.email,
+      photoUrl: firebaseUser.photoURL,
+      emailVerified: firebaseUser.emailVerified,
+      uid: firebaseUser.uid,
+    }
+    return user
+  }
 }
 
 export default AuthService
